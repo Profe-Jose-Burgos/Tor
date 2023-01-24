727a310070c33cd234244c2c5e45d5388e5b74df
Hackathon SIC 2022 <123016080+Profe-Jose-Burgos@users.noreply.github.com>
2023-01-23 20:45:14 -0500

    Add files via upload
    tuvieron dificultades para subirlo a tiempo pero se reportaron a la hora indicada con su profesor, queda como registro en este commit que lo sube el profe jose por los inconvenientes, el grupo esta en regla

---
+++ CodigoDeteccionfacialylogin.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#--------------------------------------Importamos librerias--------------------------------------------
+
+from tkinter import *
+import os
+import cv2
+from matplotlib import pyplot
+from mtcnn import MTCNN
+import numpy as np
+
+#------------------------ Crearemos una funcion que se encargara de registrar el usuario ---------------------
+
+def registrar_usuario():
+    usuario_info = usuario.get() #Obetnemos la informacion alamcenada en usuario
+    contra_info = contra.get() #Obtenemos la informacion almacenada en contra
+
+    archivo = open(usuario_info, "w") #Abriremos la informacion en modo escritura
+    archivo.write(usuario_info + "\n")   #escribimos la info
+    archivo.write(contra_info)
+    archivo.close()
+
+    #Limpiaremos los text variable
+    usuario_entrada.delete(0, END)
+    contra_entrada.delete(0, END)
+
+    #Ahora le diremos al usuario que su registro ha sido exitoso
+    Label(pantalla1, text = "Registro Convencional Exitoso", fg = "green", font = ("Calibri",11)).pack()
+    
+
+#--------------------------- Funcion para almacenar el registro facial --------------------------------------
+    
+def registro_facial():
+    #Vamos a capturar el rostro
+    cap = cv2.VideoCapture(0)               #Elegimos la camara con la que vamos a hacer la deteccion
+    while(True):
+        ret,frame = cap.read()              #Leemos el video
+        cv2.imshow('Registro Facial',frame)         #Mostramos el video en pantalla
+        if cv2.waitKey(1) == 27:            #Cuando oprimamos "Escape" rompe el video
+            break
+
